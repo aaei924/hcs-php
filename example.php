4fb56be2b6473a5f7c2f83fdb7ad571541be3cc4
Hyunseung Park <aaei924@gmail.com>
2022-05-10 20:02:01 +0900

    Update example.php

--- example.php
+++ example.php
@@ -1,17 +1,15 @@
 <?php
+/**
+* 라이브러리 사용 예시
+*/
 require 'HCSLib.php';
-$org = 'J100000000';
-$reg = 'goe';
-$name = '이름;
-$birth = '000000';
-$pass = '0000';
 
-$fu = HCS::findUser($org, $name, $birth, 'school', $reg);
-$vp = HCS::validatePassword($fu[0]['token'], $pass, $reg);
+// 학교이름과 지역은 풀네임으로, 모든 인자는 String으로
+$HCS = new HCS('한국고등학교', '홍길동', '040101', 'school', '서울특별시', '1234');
 
-$WAF = substr($vp[1], strpos($vp[1], 'WAF='), 37);
-$_JSESSIONID = substr($vp[1], strpos($vp[1], '_JSESSIONID='), 121);
+$HCS->findUser();
+$HCS->selectUserGroup();
+$res = HCS->registerServey();
 
-$rs = HCS::registerServey($vp[0], $name, $reg, $WAF.$_JSESSIONID);
-
-echo json_encode(['status' => 'Success', 'result' => $rs[0]]);
+if($res[0]['registerDtm'])
+    echo '자가진단 완료';
