0dcf6a92bc901fe9e03f8ceea56f2485b11a6c5e
PRASEOD- <aaei924@gmail.com>
2022-05-10 19:00:28 +0900

    SEED 암호화 구현성공

--- Transkey.php
+++ Transkey.php
@@ -1,5 +1,6 @@
 <?php
-require __dir__.'/KISA_SEED_CBC.php';
+require __dir__.'/SEED_CBC_HCS.php';
+
 class TransKey
 {
     private static string $delimiter = '$';
@@ -17,15 +18,15 @@ class TransKey
         // requestToken
         $getToken = self::fetch($transkeyServlet.'?op=getToken');
         preg_match('/TK_requestToken=\'?([0-9a-fA-F]*)\'?;/', $getToken, $rTmatch);
-        self::$token = $rTmatch[1];
+        $token = $rTmatch[1];
 
         // session key
-        $this->genSessionKey = bin2hex(random_bytes(16));
-        $this->sessionKey = array_map('hexdec', str_split($this->genSessionKey));
+        $genSessionKey = bin2hex(random_bytes(16));
+        $this->sessionKey = array_map('hexdec', str_split($genSessionKey));
 
         $certificate = self::fetch($transkeyServlet, http_build_query([
             'op' => 'getPublicKey',
-            'TK_requestToken' => self::$token
+            'TK_requestToken' => $token
         ]));
         $publicKey = openssl_pkey_get_public(openssl_x509_read(
             "-----BEGIN CERTIFICATE-----\n".
@@ -33,8 +34,8 @@ class TransKey
             "\n-----END CERTIFICATE-----"
         ));
         
-        openssl_public_encrypt($this->genSessionKey, $encrypted, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
-        $this->encSessionKey = bin2hex($encrypted);
+        openssl_public_encrypt($genSessionKey, $encrypted, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
+        $encSessionKey = bin2hex($encrypted);
         
         // get initTime
         $getInitTime = self::fetch($transkeyServlet.'?op=getInitTime');
@@ -43,134 +44,75 @@ class TransKey
         $this->initTime = $iTmatch[1];
         //$this->decInitTime = $dITmatch[1];
 
-        $this->keyIndex = self::fetch($transkeyServlet, http_build_query([
+        $keyIndex = self::fetch($transkeyServlet, http_build_query([
             'op' => 'getKeyIndex',
             'name' => 'password',
             'keyboardType' => 'number',
             'initTime' => $this->initTime
-        /*
-            'keyType' => 'single',
-            'fieldType' => 'password',
-            'inputName' => 'password',
-            'parentKeyboard' => 'false',
-            'transkeyUuid' => $this->uuid,
-            'exE2E' => 'false',
-            'TK_requestToken' => self::$token,
-            'isCrt' => 'false',
-            'allocationIndex' => '3011907012',
-            'keyIndex' => '',
-            'talkBack' => 'true'
-        */
+            /*'keyType' => 'single',
+              'fieldType' => 'password',
+              'inputName' => 'password',
+              'parentKeyboard' => 'false',
+              'transkeyUuid' => $this->uuid,
+              'exE2E' => 'false',
+              'TK_requestToken' => $token,
+              'isCrt' => 'false',
+              'allocationIndex' => '3011907012',
+              'keyIndex' => '',
+              'talkBack' => 'true'
+            */
         ]));
 
         $this->keys = explode(',', self::fetch($transkeyServlet, http_build_query([
             'op' => 'getDummy',
             'keyboardType' => 'number',
             'fieldType' => 'password',
-            'keyIndex' => $this->keyIndex,
+            'keyIndex' => $keyIndex,
             'talkBack' => 'true'
-        /*
-            'name' => $name,
-            'keyType' => 'single',
-            'inputName' => $inputName,
-            'transkeyUuid' => $this->uuid,
-            'exE2E' => 'false',
-            'isCrt' => 'false',
-            'allocationIndex' => '3011907012',
-            'initTime' => $this->initTime,
-            'TK_requestToken' => self::$token,
-            'dummy' => 'undefined',
-        */
+            /*'name' => $name,
+              'keyType' => 'single',
+              'inputName' => $inputName,
+              'transkeyUuid' => $this->uuid,
+              'exE2E' => 'false',
+              'isCrt' => 'false',
+              'allocationIndex' => '3011907012',
+              'initTime' => $this->initTime,
+              'TK_requestToken' => $token,
+              'dummy' => 'undefined',
+            */
         ])));
 
-        $this->enc = implode('', array_map(function($n) {
+        $enc = implode('', array_map(function($n) {
             list($x, $y) = self::$keysXY[array_search($n, $this->keys)];
-            return self::$delimiter . self::SeedEnc($x.' '.$y, $this->sessionKey, $this->initTime);
+            return self::$delimiter . SEED::encrypt($x.' '.$y, $this->sessionKey, $this->initTime);
         }, str_split($password)));
         
         for ($j=4; $j<128; $j++) {
-            $this->enc .= self::$delimiter . self::SeedEnc('# 0 0', $this->sessionKey, $this->initTime);
+            $enc .= self::$delimiter . SEED::encrypt('# 0 0', $this->sessionKey, $this->initTime);
         }
-        
+
+        $hmac = hash_hmac('sha256', $enc, $genSessionKey);
+
         // get keyinfo
-        /*
-        $this->keyInfo = self::fetch($transkeyServlet, http_build_query([
+        /*$this->keyInfo = self::fetch($transkeyServlet, http_build_query([
             'op' => 'getKeyInfo',
             'key' => $this->key,
             'transkeyUuid' => $this->uuid,
             'useCert' => 'true',
-            'TK_requestToken' => self::$token,
+            'TK_requestToken' => $token,
             'mode' => 'common'
         ]));
         */
-    }
 
-    public function finalize()
-    {
-        $this->hmac = hash_hmac('sha256', $this->enc, $this->genSessionKey);
-    }
-
-    private static $encDelimiter = ',';
-    public static function SeedEnc(string $geo, array $sessionKey, string $initTime)
-    {
-        $iv = ['4d', '6f', '62', '69', '6c', '65', '54', '72', '61', '6e', '73', '4b', '65', '79', '31', '30'];
-        $tSize = 48;
-        $inData = array_pad([], $tSize, 0);
-        $geolen = strlen($geo);
-        
-        for($i=0; $i<$geolen; ++$i) {
-            if(in_array($geo[$i], ['l', 'u', '#', ' '])) {
-                $inData[$i] = mb_ord($geo[$i]);
-            } else {
-                $inData[$i] = $geo[$i];
-            }
-        }
-
-        $inData[++$i] = 32;
-        $iTlen = strlen($initTime);
-        for ($k=0; $k<$iTlen; ++$k) {
-            if(preg_match('/^[a-z]$/', $initTime[$k]))
-                $inData[$i++] = mb_ord($initTime[$k]); // Alphabet
-            else
-                $inData[$i++] = $initTime[$k]; // Number
-        }
-
-        $inData[$i++] = 32;
-        $inData[$i++] = 37;
-        
-        $iv = array_map('hexdec', $iv);
-	
-        
-        $roundKey = KISA_SEED_CBC::SeedRoundKey(KISA_ENC_DEC::KISA_ENCRYPT, $sessionKey, $iv);
-        $outData = KISA_SEED_CBC::SEED_CBC_Encrypt($roundKey, $inData, 0, $tSize);
-
-		$encodedData = array_pad([], $tSize, 0);
-		$encodedDataString = "";
-
-		for ($k = 0; $k < $tSize; $k++) {
-			if (self::$encDelimiter === null)
-				$encodedData[$k] = dechex(intval($outData[$k]));
-			else
-				$encodedDataString .= dechex(intval($outData[$k])) . self::$encDelimiter;
-		}
-
-		if (self::$encDelimiter === null)
-			return $encodedData;
-		else
-			return substr($encodedDataString, 0, strlen($encodedDataString) - 1);
-    }
-    
-    public function json(): array
-    {
-        return [
+        $this->json = [
             'raon' => [[
                 'id' => 'password',
-                'enc' => $this->enc,
-                'hmac' => $this->hmac,
+                'enc' => $enc,
+                'hmac' => $hmac,
                 'keyboardType' => 'number',
-                'keyIndex' => $this->keyIndex,
+                'keyIndex' => $keyIndex,
                 'fieldType' => 'password',
-                'seedKey' => $this->encSessionKey,
+                'seedKey' => $encSessionKey,
                 //'initTime' => $this->initTime,
                 //'ExE2E' => 'false'
             ]]
