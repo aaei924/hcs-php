42760b9239709884a6a8ba323bcbaa853c637d85
Hyunseung Park <aaei924@gmail.com>
2023-01-13 18:28:06 +0900

    resolve dynamic property issue

--- HCSLib.php
+++ HCSLib.php
@@ -8,44 +8,52 @@ enum HTTPRequestMethods: int
 /**
  * 교육부 건강상태 자가진단 라이브러리
  * MOE Health Check System library.
- * @author Hyunseung Park (aaei924)
+ * @author PRASEOD- (aaei924)
  * @version 1.9.11
  */
 class HCS
 {
     /**
-    * 등교가 불가능하면(1, 3번 '예' 또는 2번 '양성) N, 아니면 Y
-    */
+     * 등교가 불가능하면(1, 3번 '예' 또는 2번 '양성) N, 아니면 Y
+     */
     private static string $rspns00 = 'Y';
     
     /**
-    * 1. 학생 본인이 코로나19 감염에 의심되는 아래의 임상증상이 있나요?: "1"=아니오, "2"=예
-    */
+     * 1. 학생 본인이 코로나19 감염에 의심되는 아래의 임상증상이 있나요?: "1"=아니오, "2"=예
+     */
     private static string $rspns01 = '1';
     
     /**
-    * 3. 학생 본인이 PCR 등 검사를 받고 그 결과를 기다리고 있나요?: "1"=아니오, "0"=예
-    */
+     * 3. 학생 본인이 PCR 등 검사를 받고 그 결과를 기다리고 있나요?: "1"=아니오, "0"=예
+     */
     private static string $rspns02 = '1';
     
     /**
-    * 2. 학생은 오늘(어제 저녁 포함) 신속항원검사(자가진단)를 실시했나요?: "1"=실시하지 않음, null=실시함
-    */
+     * 2. 학생은 오늘(어제 저녁 포함) 신속항원검사(자가진단)를 실시했나요?: "1"=실시하지 않음, null=실시함
+     */
     private static string|null $rspns03 = '1';
     
     /**
-    * 2. 학생은 오늘(어제 저녁 포함) 신속항원검사(자가진단)를 실시했나요?: null=실시하지 않음, "0"=음성, "1"=양성
-    */
+     * 2. 학생은 오늘(어제 저녁 포함) 신속항원검사(자가진단)를 실시했나요?: null=실시하지 않음, "0"=음성, "1"=양성
+     */
     private static string|null $rspns07 = null;
     
     /**
-    * 내용없는 문항
-    */
+     * 내용없는 문항
+     */
     private static $rspns04, $rspns05, $rspns06, $rspns08, $rspns09, $rspns10, $rspns11, $rspns12, $rspns13, $rspns14, $rspns15 = null;
 
+    /**
+     * 기타 파라미터
+     */
+    public $orgCode, $juOrgCode, $lctnScCode, $baseUrl, $searchKey, $WAF, $_JSESSIONID, $userGroupToken, $userToken, $userPNo, $stdntYn, $pInfAgrmYn;
+
+    /**
+     * User-Agent is set to iOS App.
+     */
     private static array $headers = [
         'Content-Type: application/json',
-        'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13B143',
+        'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13B143',
         'Origin: https://hcs.eduro.go.kr',
         'Referer: https://hcs.eduro.go.kr/'
     ];
@@ -71,10 +79,10 @@ class HCS
     ) {}
     
     /**
-    * RSA Encryption (RSA/ECB/PKCS1Padding)
-    * @param  string $text  text to encrypt
-    * @return string        encrypted text
-    */
+     * RSA Encryption (RSA/ECB/PKCS1Padding)
+     * @param  string $text  text to encrypt
+     * @return string        encrypted text
+     */
     private static function RSAEncrypt(string $text): string
     {
         $publicKey = 
@@ -86,13 +94,13 @@ class HCS
     }
 
     /**
-    * send HTTP request with curl
-    * @param HTTPRequestMethods $method request method
-    * @param string $url     request URL
-    * @param array  $headers HTTP headers
-    * @param array  $data    form data to submit
-    * @return array|null     returns null when error
-    */
+     * send HTTP request with curl
+     * @param HTTPRequestMethods $method request method
+     * @param string $url     request URL
+     * @param array  $headers HTTP headers
+     * @param array  $data    form data to submit
+     * @return array|null     returns null when error
+     */
     private static function request(HTTPRequestMethods $method, string $url, array $headers=[], array $data=[], $decode=1): array|string|null
     {
         $ch = curl_init();
@@ -114,6 +122,7 @@ class HCS
         $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
         curl_close($ch);
 
+        // POST
         if($method->value === 1){
             $headers = substr($response, 0, $header_size);
             $body = substr($response, $header_size);
@@ -151,17 +160,22 @@ class HCS
 
     /**
      * find user data
-     * -----------------
+     * ----
+     * ----
+     * gets userGroupToken
+     * 
      * hcs 1.9.10 update:
      * change endpoint to /v3/
-    */
+     */
     public function findUser(): void
     {
         require __DIR__.'/TransKey.php';
 
         $this->getSchoolInfo();
 
-        $raon = new Transkey($this->password);
+        $raon = new Transkey($this->password, 'https://hcs.eduro.go.kr', 'number', 'password', 'password', 'password', 'single');
+
+        $raon->makeData();
         
         $data = [
             'orgCode' => $this->orgCode,
@@ -169,7 +183,19 @@ class HCS
             'birthday' => self::RSAEncrypt($this->birthday),
             'loginType' => $this->loginType,
             'stdntPNo' => null,
-            'password' => json_encode($raon->json),
+            'password' => json_encode([
+                'raon' => [[
+                    'id' => 'password',
+                    'enc' => $raon->enc,
+                    'hmac' => $raon->hmac,
+                    'keyboardType' => $raon->keyboardType,
+                    'keyIndex' => $raon->keyIndex,
+                    'fieldType' => 'password',
+                    'seedKey' => $raon->encSessionKey,
+                    'initTime' => $raon->initTime,
+                    'ExE2E' => 'false'
+                ]]
+            ]),
             'lctnScCode' => $this->lctnScCode,
             'deviceUuid' => '',
             'makeSession' => true,
@@ -178,36 +204,47 @@ class HCS
 
         $res = self::request(self::POST, $this->baseUrl.'/v3/findUser', self::$headers, $data);
         $this->stdntYn = $res[0]['stdntYn'];
-        $this->token = $res[0]['token'];
+        $this->userGroupToken = $res[0]['token'];
         $this->pInfAgrmYn = $res[0]['pInfAgrmYn'];
         $this->WAF = substr($res[1], strpos($res[1], 'WAF='), 37);
         $this->_JSESSIONID = substr($res[1], strpos($res[1], '_JSESSIONID='), 121);
+        
+        //var_dump($res);
     
         //if($res[0]['isError'])
         //    throw new ErrorException($res[0]['statusCode'].'/'.$res[0]['errorCode'].':'.$res[0]['message']);
     }
     
     /**
-    * get registered user list and change token
-    */
+     * ----
+     * ----
+     * uses userGroupToken
+     * gets userToken
+     * 
+     * get registered user list and change token
+     */
     public function selectUserGroup(): void
     {
-        self::$headers[4] = 'Authorization: '.$this->token;
+        self::$headers[4] = 'Authorization: '.$this->userGroupToken;
         self::$headers[5] = 'Cookie: '.$this->WAF.$this->_JSESSIONID;
         
         $data = [];
         
         $res = self::request(self::POST, $this->baseUrl.'/v2/selectUserGroup', self::$headers, $data)[0];
         
-        $this->token = $res[0]['token'];
+        $this->userToken = $res[0]['token'];
         $this->userPNo = $res[0]['userPNo'];
         
-        self::$headers[4] = 'Authorization: '.$this->token;
+        self::$headers[4] = 'Authorization: '.$this->userToken;
     }
     
     /**
-    * get user details
-    */
+     * ----
+     * ----
+     * uses userToken
+     * 
+     * get user details
+     */
     public function getUserInfo(): void
     {
         $data = [
@@ -219,9 +256,9 @@ class HCS
     }
     
     /**
-    * get hcs client version
-    * @return string hcs client version
-    */
+     * get hcs client version
+     * @return string hcs client version
+     */
     public static function getClientVersion(): string
     {
         file_get_contents('https://hcs.eduro.go.kr/');
@@ -236,9 +273,13 @@ class HCS
     }
     
     /**
-    * submit health check survey content
-    * @return array|null returns null when error
-    */
+     * ----
+     * ----
+     * uses userToken
+     * 
+     * submit health check survey content
+     * @return array|null returns null when error
+     */
     public function registerServey(): array|null
     {
         $data = [
@@ -258,7 +299,7 @@ class HCS
             'rspns13' => self::$rspns13,
             'rspns14' => self::$rspns14,
             'rspns15' => self::$rspns15,
-            'upperToken' => $this->token,
+            'upperToken' => $this->userToken,
             'upperUserNameEncpt' => $this->name,
             'deviceUuid' => '',
             'clientVersion' => self::getClientVersion()
@@ -266,48 +307,93 @@ class HCS
         
         return self::request(self::POST, $this->baseUrl.'/registerServey', self::$headers, $data);
     }
-    
-    /*
-    public function searchSchool()
+
+    /**
+     * < in construction >
+     * 
+     * update agreement of processing personal information
+     */
+    public function updateAgreement(): void
     {
-        $headers = [
-            'Content-Type: application/json',
-            'Authorization: '.$this->token,
-            'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13B143',
-            'Origin: https://hcs.eduro.go.kr',
-            'Referer: https://hcs.eduro.go.kr/'
-        ];
-        
-        $data = [
-            'orgCode' => $orgCode,
-            'userPNo' => $userPNo
-        ]; 
-        
-        return self::requestPOST($url, $headers, $data);
+        self::request(self::POST, $this->baseUrl.'/v2/updatePInfAgrmYn', self::$headers);
     }
-    
-    public function joinClassList()
-    {
-        
-        $data = [];
-        
-        return self::requestPOST($this->region.'hcs.eduro.go.kr/joinClassList', self::$headers, $data);
-    }
-    
-    public function join($token, $orgCode, $grade, $classNm, $classCode, $url)
+
+    /**
+     * register password of user
+     */
+    public function registerPassword(string $password): void
     {
         $data = [
-            'orgCode' => $this->orgCode,
-            'grade' => $this->grade,
-            'classNm' => $this->classNm,
-            'classCode' => $this->classCode
+            'password' => self::RSAEncrypt($password),
+            'deviceUuid' => '',
+            'upperToken' => $this->userGroupToken
         ];
-        
-        return self::requestPOST($this->region.'hcs.eduro.go.kr/join', self::$headers, $data);
+
+        self::request(self::POST, $this->baseUrl.'/v2/registerPassword', self::$headers, $data);
     }
-    
-    public function joinDetail($data)
+
+    /**
+     * ----
+     * ----
+     * uses userGroupToken
+     * 
+     * change password of user
+     */
+    public function changePassword(string $before, string $after): void
     {
-        return self::requestPOST($this->region.'hcs.eduro.go.kr/joinDetail', self::$headers, $data);
-    }*/
+        $data = [
+            'password' => self::RSAEncrypt($before),
+            'newPassword' => self::RSAEncrypt($after)
+        ];
+
+        self::request(self::POST, $this->baseUrl.'/v2/changePassword', self::$headers, $data);
+    }
+
+    /**
+     * ----
+     * ----
+     * uses userGroupToken
+     * 
+     * get notice list
+     */
+    public function selectNoticeList(int $pgcnt, int $licnt): array
+    {
+        return self::request(self::GET, $this->baseUrl.'/v2/selectNoticeList?currentPageNumber='.$pgcnt.'&listCount='.$licnt, self::$headers);
+    }
+
+    /**
+     * ----
+     * ----
+     * uses userGroupToken
+     * 
+     * get notice content
+     */
+    public function selectNotice(string $idx): array
+    {
+        return self::request(self::GET, $this->baseUrl.'/v2/selectNotice?idxNtc='.$idx, self::$headers);
+    }
+
+    /**
+     * ----
+     * ----
+     * uses userToken
+     * 
+     * get external survey list
+     */
+    public function selectExtSurveyList(): array
+    {
+        return self::request(self::POST, $this->baseUrl.'/v2/selectExtSurveyList', self::$headers);
+    }
+
+    /**
+     * ----
+     * ----
+     * uses userGroupToken
+     * 
+     * get hospital list
+     */
+    public function selectHospital(string $lctnScNm, string $keyword=''): array
+    {
+        return self::request(self::GET, $this->baseUrl.'/v2/selectHospitals?lctnScNm='.urlencode($lctnScNm).'&hsptNm='.urlencode($keyword), self::$headers);
+    }
 }
